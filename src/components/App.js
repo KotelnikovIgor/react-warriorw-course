7aec925614f05e7ee70fde2daa47278cca4a9381
Kotelnikov Igor <digikot1996@gmail.com>
2020-03-18 09:50:22 +0200

    page

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,9 @@ class App extends Component {
     this.state = {
       movies: [],
       moviesWillWatch: [],
-      sort_by: "revenue.desc"
+      sort_by: "revenue.desc",
+      page: 1,
+      total_page: ""
     };
     console.log("App constructor");
   }
@@ -23,26 +25,31 @@ class App extends Component {
     // console.log("after fetch");
   }
   componentDidUpdate(prevProps, prevState) {
-    console.log("App didUpdate");
-    console.log("prev", prevProps, prevState);
-    console.log("this", this.props, this.state);
-    if (prevState.sort_by !== this.state.sort_by) {
+    const { sort_by, page } = this.state;
+    // console.log("App didUpdate");
+    // console.log("prev", prevProps, prevState);
+    // console.log("this", this.props, this.state);
+    if (prevState.sort_by !== sort_by || prevState.page !== page) {
       this.getMovies();
     }
   }
 
   getMovies = () => {
+    const { sort_by, page } = this.state;
+    const qwerySrting = "/discover/movie";
+
     fetch(
-      `${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=${this.state.sort_by}`
+      `${API_URL}${qwerySrting}?api_key=${API_KEY_3}&sort_by=${sort_by}&page=${page}`
     )
       .then(response => {
         console.log("then", response);
         return response.json();
       })
       .then(data => {
-        console.log("data ", data);
+        console.log("fetch data ", data);
         this.setState({
-          movies: data.results
+          movies: data.results,
+          total_page: data.total_pages
         });
       });
   };
@@ -79,8 +86,24 @@ class App extends Component {
     });
   };
 
+  handelPrewPage = () => {
+    const { page } = this.state;
+    if (page === 1) {
+      return;
+    } else {
+      this.setState({
+        page: page + -1
+      });
+    }
+  };
+  handelNextPage = () => {
+    const { page } = this.state;
+    this.setState({
+      page: page + 1
+    });
+  };
   render() {
-    const { movies, moviesWillWatch, sort_by } = this.state;
+    const { movies, moviesWillWatch, sort_by, page, total_page } = this.state;
     console.log(this);
     console.log(movies);
     console.log("App render");
@@ -102,6 +125,29 @@ class App extends Component {
           </div>
           <div className="col-3">
             <MovieListWillWatch moviesWillWatch={moviesWillWatch} />
+          </div>
+          <div className="col-12 mb-2 ml-3">
+            <div className="row">
+              <button
+                type="button"
+                onClick={this.handelPrewPage}
+                className="btn btn-secondary"
+              >
+                Prew
+              </button>
+              <div className="ml-3 mr-3">
+                <h4>
+                  Movie Page: {page}/{total_page}
+                </h4>
+              </div>
+              <button
+                type="button"
+                onClick={this.handelNextPage}
+                className="btn btn-secondary"
+              >
+                Next
+              </button>
+            </div>
           </div>
         </div>
       </div>
