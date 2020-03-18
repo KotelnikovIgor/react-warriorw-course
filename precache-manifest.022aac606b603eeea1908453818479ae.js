124a17b985c67760a8678e35c5102c9f0c386e1a
Kotelnikov Igor <digikot1996@gmail.com>
2020-03-18 10:20:26 +0200

    Updates

---
+++ precache-manifest.022aac606b603eeea1908453818479ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20aed7f32a89e034035ce5660c869a36",
+    "url": "/react-warriorw-course/index.html"
+  },
+  {
+    "revision": "b165199476b687e55d54",
+    "url": "/react-warriorw-course/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1581b5877fb1a881cf58",
+    "url": "/react-warriorw-course/static/css/main.4a014b45.chunk.css"
+  },
+  {
+    "revision": "b165199476b687e55d54",
+    "url": "/react-warriorw-course/static/js/2.fb48d99d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-warriorw-course/static/js/2.fb48d99d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1581b5877fb1a881cf58",
+    "url": "/react-warriorw-course/static/js/main.85fbb151.chunk.js"
+  },
+  {
+    "revision": "90475c3ade2711b8d10b",
+    "url": "/react-warriorw-course/static/js/runtime-main.1ae96570.js"
+  }
+]);
